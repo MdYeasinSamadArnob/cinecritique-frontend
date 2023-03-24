9e23086ecd3750b386b9e07176984991dcd4f81b
MdYeasinSamadArnob <yeasinsamadarnob@gmail.com>
2023-03-24 17:40:29 +0600

    SignUp Created

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React from "react";
 import { Routes, Route } from "react-router-dom";
 import { Home } from "./pages";
 import SignIn from "./pages/SignIn/SignIn";
+import SignUp from "./pages/SignUp/SignUp";
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
       <Route path="/signin" element={<SignIn/>} />
+      <Route path="/signup" element={<SignUp/>} />
     </Routes>
   );
 }
